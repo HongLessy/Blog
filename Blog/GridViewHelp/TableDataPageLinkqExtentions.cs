046018f8c0700654c3b1ad5b31787c5bc81e3cc4
HongLessy <xdudu520@gmail.com>
2016-06-18 22:31:00 +0800

    5th push
    本次提交增加了GridViewHelper.cs和TableData相关类文件，是打算制作一个自定义表格显示数据库数据，完成了第一部分。

---
+++ Blog/GridViewHelp/TableDataPageLinkqExtentions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridViewHelp
+{
+    /// <summary>
+    /// 拓展方法，目标位HtmlHelper对象添加表数据显示的方法
+    /// </summary>
+    public static class TableDataPageLinkqExtentions
+    {
+        public static TableDataPagedList<T> ToPagedList<T>(this IQueryable<T> allitems,int? pageIndex,int pageSize,string identityColumnName,string sortExpression)
+        {
+            var truePageIndex = pageIndex ?? 0;
+            var itemIndex=truePageIndex*pageSize;
+            var pageOfItems = allitems.Skip(itemIndex).Take(pageSize);
+
+            return new TableDataPagedList<T>(pageOfItems,truePageIndex,pageSize,allitems.Count(),identityColumnName,sortExpression); 
+        }
+        public static TableDataPagedList<T> ToPagedList<T>(this IQueryable<T> allitems, int? pageIndex, int pageSize,string identityColumnName)
+        {           
+            return ToPagedList(allitems, pageIndex, pageSize,identityColumnName, null);
+        }
+        public static TableDataPagedList<T> ToPagedList<T>(this IQueryable<T> allitems, int? pageIndex, int pageSize)
+        {
+            return ToPagedList(allitems, pageIndex, pageSize, null,string.Empty);
+        }
+    }
+}
